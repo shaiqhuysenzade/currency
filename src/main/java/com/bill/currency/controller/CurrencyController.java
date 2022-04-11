62eec3bcb2dd009fc894a74200c76578e6729f61
shaiqhuysenzade <shaiq.huseynzade@geeksforless.com>
2022-04-12 02:43:44 +0400

    --add request mapping /rate--

--- src/main/java/com/bill/currency/controller/CurrencyController.java
+++ src/main/java/com/bill/currency/controller/CurrencyController.java
@@ -4,10 +4,12 @@ import com.bill.currency.dto.CurrencyResponseDto;
 import com.bill.currency.service.CurrencyService;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @AllArgsConstructor
+@RequestMapping("/rate")
 public class CurrencyController {
 
     private final CurrencyService currencyService;
